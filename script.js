dea5ffe3c8f899d188e047d9eab5c94bf3037e35
Royal <royalhuseyn902@gmail.com>
2023-11-21 00:32:49 +0400

    Create Gameboard modular pattern Since there is only one instance of Gameboard, create it as IIFE so that no further instances can be created from Gameboard. Add gameboard array and displayBoard inside Gameboard

---
+++ script.js
@@ -0,0 +1,11 @@
+const gameboard = (function() {
+  const gameboard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const displayBoard = () => {
+    console.log(` ${gameboard[0]} | ${gameboard[1]} | ${gameboard[2]} ` + 
+                `\n---+---+---\n` + 
+                ` ${gameboard[3]} | ${gameboard[4]} | ${gameboard[5]} ` + 
+                `\n---+---+---\n` + 
+                ` ${gameboard[6]} | ${gameboard[7]} | ${gameboard[8]} `);
+  };
+  return {gameboard, displayBoard};
+})();
