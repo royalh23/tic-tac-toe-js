82e95ee06dbcbed9083056d50bd1422a4546b448
Royal <royalhuseyn902@gmail.com>
2023-11-21 00:34:53 +0400

    Capitalize Gameboard factory

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gameboard = (function() {
+const Gameboard = (function() {
   const gameboard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   const displayBoard = () => {
     console.log(` ${gameboard[0]} | ${gameboard[1]} | ${gameboard[2]} ` + 
