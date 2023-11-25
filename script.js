103fb9c8421c4cc969b7239df80e431efcb30cbd
Royal <royalhuseyn902@gmail.com>
2023-11-25 21:32:02 +0400

    Change factories' names

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const Game = (function() {
+const game = (function() {
   let gameRound = 1;
   let playerOne;
   let playerTwo;
 
   function checkGame(player) {
-    if (Gameboard.gameboard[0] == Gameboard.gameboard[1] && Gameboard.gameboard[1] == Gameboard.gameboard[2] ||
-        Gameboard.gameboard[0] == Gameboard.gameboard[3] && Gameboard.gameboard[3] == Gameboard.gameboard[6] ||
-        Gameboard.gameboard[2] == Gameboard.gameboard[5] && Gameboard.gameboard[5] == Gameboard.gameboard[8] ||
-        Gameboard.gameboard[6] == Gameboard.gameboard[7] && Gameboard.gameboard[7] == Gameboard.gameboard[8] ||
-        Gameboard.gameboard[0] == Gameboard.gameboard[4] && Gameboard.gameboard[4] == Gameboard.gameboard[8] ||
-        Gameboard.gameboard[2] == Gameboard.gameboard[4] && Gameboard.gameboard[4] == Gameboard.gameboard[6] ||
-        Gameboard.gameboard[3] == Gameboard.gameboard[4] && Gameboard.gameboard[4] == Gameboard.gameboard[5] ||
-        Gameboard.gameboard[1] == Gameboard.gameboard[4] && Gameboard.gameboard[4] == Gameboard.gameboard[7]) {
-          DisplayController.dialogText.textContent = `${player.name} wins!`;
-          DisplayController.gameDialog.showModal();
+    if (gameboard.gameboard[0] == gameboard.gameboard[1] && gameboard.gameboard[1] == gameboard.gameboard[2] ||
+        gameboard.gameboard[0] == gameboard.gameboard[3] && gameboard.gameboard[3] == gameboard.gameboard[6] ||
+        gameboard.gameboard[2] == gameboard.gameboard[5] && gameboard.gameboard[5] == gameboard.gameboard[8] ||
+        gameboard.gameboard[6] == gameboard.gameboard[7] && gameboard.gameboard[7] == gameboard.gameboard[8] ||
+        gameboard.gameboard[0] == gameboard.gameboard[4] && gameboard.gameboard[4] == gameboard.gameboard[8] ||
+        gameboard.gameboard[2] == gameboard.gameboard[4] && gameboard.gameboard[4] == gameboard.gameboard[6] ||
+        gameboard.gameboard[3] == gameboard.gameboard[4] && gameboard.gameboard[4] == gameboard.gameboard[5] ||
+        gameboard.gameboard[1] == gameboard.gameboard[4] && gameboard.gameboard[4] == gameboard.gameboard[7]) {
+          displayController.dialogText.textContent = `${player.name} wins!`;
+          displayController.gameDialog.showModal();
         } else if (gameRound == 9) {
-          DisplayController.dialogText.textContent = "It's a tie!";
-          DisplayController.gameDialog.showModal();
+          displayController.dialogText.textContent = "It's a tie!";
+          displayController.gameDialog.showModal();
         }
   }
 
@@ -25,23 +25,23 @@ const Game = (function() {
       resetGame();
     } else {
       startBtn.textContent = "Restart";
-      DisplayController.getNames();
-      playerOne = Player(DisplayController.playerNames[0], "X");
-      playerTwo = Player(DisplayController.playerNames[1], "O");
-      DisplayController.addEventListeners();
+      displayController.getNames();
+      playerOne = createPlayer(displayController.playerNames[0], "X");
+      playerTwo = createPlayer(displayController.playerNames[1], "O");
+      displayController.addEventListeners();
     }
   }
 
   function placeMark(e) {
     if (!(e.target.textContent === "X" || e.target.textContent === "O")) {
       if (gameRound % 2 === 1) {
-        Gameboard.gameboard[e.target.dataset.index] = playerOne.mark;
-        DisplayController.displayBoard();
+        gameboard.gameboard[e.target.dataset.index] = playerOne.mark;
+        displayController.displayBoard();
         checkGame(playerOne);
         gameRound++;
       } else {
-        Gameboard.gameboard[e.target.dataset.index] = playerTwo.mark;
-        DisplayController.displayBoard();
+        gameboard.gameboard[e.target.dataset.index] = playerTwo.mark;
+        displayController.displayBoard();
         checkGame(playerTwo);
         gameRound++;
       }
@@ -49,29 +49,29 @@ const Game = (function() {
   }
 
   function resetGame() {
-    DisplayController.nameInputs.forEach(input => input.value = "");
+    displayController.nameInputs.forEach(input => input.value = "");
     startBtn.textContent = "Start";
     gameRound = 1;
-    DisplayController.removeEventListeners();
-    Gameboard.gameboard = ["", " ", "  ", "   ", "    ", "     ", "      ", "       ", "        ",];
-    DisplayController.displayBoard();
-    DisplayController.gameDialog.close();
+    displayController.removeEventListeners();
+    gameboard.gameboard = ["", " ", "  ", "   ", "    ", "     ", "      ", "       ", "        ",];
+    displayController.displayBoard();
+    displayController.gameDialog.close();
   }
 
   return {checkGame, runGame, placeMark, resetGame};
 })();
 
-const Gameboard = (function() {
+const gameboard = (function() {
   const gameboard = ["", " ", "  ", "   ", "    ", "     ", "      ", "       ", "        ",];
   
   return {gameboard};
 })();
 
-function Player(name, mark) {
+function createPlayer(name, mark) {
   return {name, mark};
 }
 
-const DisplayController = (function() {
+const displayController = (function() {
   const playerNames = [];
   const nameInputs = document.querySelectorAll("input");
   const boardSquares = document.querySelectorAll(".container > div");
@@ -79,10 +79,10 @@ const DisplayController = (function() {
   const newGameBtn = document.querySelector("dialog button");
   const dialogText= document.querySelector(".dialog-text");
 
-  newGameBtn.addEventListener("click", Game.resetGame);
+  newGameBtn.addEventListener("click", game.resetGame);
 
   function displayBoard() {
-    boardSquares.forEach(square => square.textContent = Gameboard.gameboard[square.dataset.index]);
+    boardSquares.forEach(square => square.textContent = gameboard.gameboard[square.dataset.index]);
   }
 
   function getNames() {
@@ -91,15 +91,15 @@ const DisplayController = (function() {
   }
 
   function addEventListeners() {
-    boardSquares.forEach(square => square.addEventListener("click", Game.placeMark));
+    boardSquares.forEach(square => square.addEventListener("click", game.placeMark));
   }
 
   function removeEventListeners() {
-    boardSquares.forEach(square => square.removeEventListener("click", Game.placeMark));
+    boardSquares.forEach(square => square.removeEventListener("click", game.placeMark));
   }
 
   return {playerNames, nameInputs, gameDialog, dialogText, newGameBtn, displayBoard, getNames, addEventListeners, removeEventListeners};
 })();
 
 const startBtn = document.querySelector(".start");
-startBtn.addEventListener("click", Game.runGame);
+startBtn.addEventListener("click", game.runGame);
